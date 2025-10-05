570433313a91ced6acbb3f6366b9e46f52ea2435
Farhan <ullah8113@gmail.com>
2025-10-05 22:12:55 +0600

    Navbar, sidebar, Theme and redux added

--- client/src/app/(components)/Navbar/page.tsx
+++ client/src/app/(components)/Navbar/page.tsx
@@ -1,11 +1,27 @@
-import { Search, Settings } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="flex items-center justify-between border-b border-gray-200 bg-white px-4 py-3 dark:border-gray-800 dark:bg-black">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[300px]">
           <Search className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 transform text-gray-400 dark:text-gray-500" />
           <input
@@ -18,6 +34,20 @@ const Navbar = () => {
 
       {/* icons */}
       <div className="flex items-center gap-2">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
           className="rounded-lg p-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800"
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Navbar from "./(components)/Navbar/page";
 import Sidebar from "./(components)/Sidebar/page";
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* Sidebar */}
       <Sidebar />
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`dark:bg-dark-bg { isSidebarCollapsed ? "" : "md:pl-64" } flex w-full flex-col bg-gray-50 md:pl-64`}
       >
         {/* navbar */}
         <Navbar />
@@ -18,4 +33,11 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
 export default DashboardWrapper;
