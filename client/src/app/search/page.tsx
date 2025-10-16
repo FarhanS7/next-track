72bad3a8352e5d3c27e8ddd1af0f40c20fc7f20e
Farhan <ullah8113@gmail.com>
2025-10-16 22:30:00 +0600

    Search and User Frontend

---
+++ client/src/app/search/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Header from "@/components/Header/page";
+import TaskCard from "@/components/TaskCard/page";
+import { useSearchQuery } from "@/state/api";
+import { debounce } from "lodash";
+import React, { useEffect, useState } from "react";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const {
+    data: searchResults,
+    isLoading,
+    isError,
+  } = useSearchQuery(searchTerm, {
+    skip: searchTerm.length < 3,
+  });
+
+  const handleSearch = debounce(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value);
+    },
+    500,
+  );
+
+  useEffect(() => {
+    return handleSearch.cancel;
+  }, [handleSearch.cancel]);
+
+  return (
+    <div className="p-8">
+      <Header name="Search" />
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-1/2 rounded border p-3 shadow"
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="p-5">
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error occurred while fetching search results.</p>}
+        {!isLoading && !isError && searchResults && (
+          <div>
+            {searchResults.tasks && searchResults.tasks?.length > 0 && (
+              <h2>Tasks</h2>
+            )}
+            {searchResults.tasks?.map((task) => (
+              <TaskCard key={task.id} task={task} />
+            ))}
+
+            {searchResults.projects && searchResults.projects?.length > 0 && (
+              <h2>Projects</h2>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
