3eab02710708b6d9a36cd6901b9a83331217b4c0
Farhan <ullah8113@gmail.com>
2025-10-17 13:10:59 +0600

    Priority Page added

---
+++ client/src/app/priority/reusablePriorityPage/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { useAppSelector } from "@/app/redux";
+import Header from "@/components/Header/page";
+import ModalNewTask from "@/components/ModalNewTask/page";
+import { Priority } from "@/state/api";
+import { GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+
+type Props = {
+  priority: Priority;
+};
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs leading-5 font-semibold text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width: 130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width: 130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width: 150,
+    renderCell: (params) => params.value.username || "Unassigned",
+  },
+];
+
+const ReusablePriorityPage = ({ priority }: Props) => {
+  const [view, setView] = useState("list");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  return (
+    <div className="m-5 p-4">
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+      />
+      <Header
+        name="Priority Page"
+        buttonComponent={
+          <button
+            className="mr-3 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={() => setIsModalNewTaskOpen(true)}
+          >
+            Add Task
+          </button>
+        }
+      />
+      <div className="mb-4 flex justify-start">
+        <button
+          className={`px-4 py-2 ${
+            view === "list" ? "bg-gray-300" : "bg-white"
+          } rounded-l`}
+          onClick={() => setView("list")}
+        >
+          List
+        </button>
+        <button
+          className={`px-4 py-2 ${
+            view === "table" ? "bg-gray-300" : "bg-white"
+          } rounded-l`}
+          onClick={() => setView("table")}
+        >
+          Table
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ReusablePriorityPage;
