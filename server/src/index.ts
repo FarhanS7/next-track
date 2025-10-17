008a00fbd28d51101ad6c5f8381aca1567132dc5
Farhan <ullah8113@gmail.com>
2025-10-17 13:18:09 +0600

    Server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,8 +32,8 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 // Server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 5000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
 export default app;
